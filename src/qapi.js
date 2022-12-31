3832abd6c1deaf0d8796c919adb6653a16300413
zeroday19 <107371184+zeroday19@users.noreply.github.com>
2022-12-31 12:49:30 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63afe118f63de4002672ac54",
+				"63afe118f63de4002672ac57"
+			]
+		},
+		"63afe118f63de4002672ac54": {
+			"id": "63afe118f63de4002672ac54",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63afe118f63de4002672ac57": {
+			"id": "63afe118f63de4002672ac57",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
